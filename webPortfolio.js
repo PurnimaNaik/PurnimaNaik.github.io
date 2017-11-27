d6f1fc18ab516eb1afb9e572181604689e16d69e
Purnima <purnima@Purnimas-MacBook-Pro.local>
2017-11-26 21:06:55 -0500

    Updated Content

--- webPortfolio.js
+++ webPortfolio.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
                 scroll_pos = $(this).scrollTop();
                 console.log(scroll_pos);
 
-                if(scroll_pos <250) {
+                if(scroll_pos <450) {
                     
                     $("#1").css('text-decoration', 'none');
                     $("#2").css('text-decoration', 'none');
@@ -42,7 +42,7 @@ $(document).ready(function(){
                     $("#6").css('text-decoration', 'none');
           }
 
-                else if(scroll_pos > 400&&scroll_pos < 600) {
+                else if(scroll_pos > 450&&scroll_pos < 850) {
                     
                     $("#1").css('text-decoration', 'underline');
                     $("#2").css('text-decoration', 'none');
@@ -51,7 +51,7 @@ $(document).ready(function(){
                     $("#5").css('text-decoration', 'none');
                     $("#6").css('text-decoration', 'none');
           }
-          else if(scroll_pos > 650&&scroll_pos < 1000) {
+          else if(scroll_pos > 850&&scroll_pos < 1600) {
                     $("#2").css('text-decoration', 'underline');
                     $("#1").css('text-decoration', 'none');
                     $("#3").css('text-decoration', 'none');
@@ -59,7 +59,7 @@ $(document).ready(function(){
                     $("#5").css('text-decoration', 'none');
                     $("#6").css('text-decoration', 'none');
           }
-          else if(scroll_pos > 1401&&scroll_pos < 1600) {
+          else if(scroll_pos > 1800&&scroll_pos < 2300) {
                     $("#3").css('text-decoration', 'underline');
                     $("#1").css('text-decoration', 'none');
                     $("#2").css('text-decoration', 'none');
@@ -67,30 +67,30 @@ $(document).ready(function(){
                     $("#5").css('text-decoration', 'none');
                     $("#6").css('text-decoration', 'none');
           }
-          else if(scroll_pos > 1800 && scroll_pos < 1900) {
+          else if(scroll_pos > 2300 && scroll_pos < 2800) {
                     $("#4").css('text-decoration', 'underline');
                     $("#1").css('text-decoration', 'none');
                     $("#3").css('text-decoration', 'none');
                     $("#2").css('text-decoration', 'none');
-                    $("#5").css('text-decoration', 'none');
-                    $("#6").css('text-decoration', 'none');
-          }
-          else if(scroll_pos > 2001 && scroll_pos < 2100) {
                     $("#5").css('text-decoration', 'underline');
-                    $("#1").css('text-decoration', 'none');
-                    $("#3").css('text-decoration', 'none');
-                    $("#4").css('text-decoration', 'none');
-                    $("#2").css('text-decoration', 'none');
-                    $("#6").css('text-decoration', 'none');
-          }
-          else if(scroll_pos > 2101) {
                     $("#6").css('text-decoration', 'underline');
-                    $("#1").css('text-decoration', 'none');
-                    $("#3").css('text-decoration', 'none');
-                    $("#4").css('text-decoration', 'none');
-                    $("#5").css('text-decoration', 'none');
-                    $("#2").css('text-decoration', 'none');
           }
+          // else if(scroll_pos > 2800 && scroll_pos < 3000) {
+          //           $("#5").css('text-decoration', 'underline');
+          //           $("#1").css('text-decoration', 'none');
+          //           $("#3").css('text-decoration', 'none');
+          //           $("#4").css('text-decoration', 'none');
+          //           $("#2").css('text-decoration', 'none');
+          //           $("#6").css('text-decoration', 'none');
+          // }
+          // else if(scroll_pos > 3000) {
+          //           $("#6").css('text-decoration', 'underline');
+          //           $("#1").css('text-decoration', 'none');
+          //           $("#3").css('text-decoration', 'none');
+          //           $("#4").css('text-decoration', 'none');
+          //           $("#5").css('text-decoration', 'none');
+          //           $("#2").css('text-decoration', 'none');
+          // }
          
                
             });
