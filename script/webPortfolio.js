8510a519ad5e3b4cf6b32db3183f82fbf8d79e3d
PurnimaNaik <naikpurnima9@gmail.com>
2019-09-09 15:57:05 -0400

    Enhanced scroll position highlight

--- script/webPortfolio.js
+++ script/webPortfolio.js
@@ -1,4 +1,38 @@
 
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function Utils() {
+
+}
+
+Utils.prototype = {
+    constructor: Utils,
+    isElementInView: function (element, fullyInView) {
+        var pageTop = $(window).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+        var elementTop = $(element).offset().top;
+        var elementBottom = elementTop + $(element).height();
+
+        if (fullyInView === true) {
+            return ((pageTop < elementTop) && (pageBottom > elementBottom));
+        } else {
+            return ((elementTop <= pageBottom-($(window).height()/2.5)) && (elementBottom >= pageTop));
+        }
+
+    }
+};
+
+var Utils = new Utils();
+
 $(document).ready(function(){
   $(document).scroll(function(){
     $("#miniIntro").animate({left: '250px'},1100);
@@ -32,50 +66,93 @@ $(document).ready(function(){
   $(document).scroll(function() {
     scroll_pos = $(this).scrollTop();
 
-    if ($('#skills').is(':visible')) {
-      console.log("skills visible")
-  } else {
-    console.log("skills not visible")
-  }
-
-    if(scroll_pos <450) {
-
+    var isElementInView = Utils.isElementInView($('#intro'), false);
+    if (isElementInView) {
       $("#1").css('text-decoration', 'none');
       $("#2").css('text-decoration', 'none');
+      $("#3").css('text-decoration', 'none');
       $("#4").css('text-decoration', 'none');
       $("#5").css('text-decoration', 'none');
-      $("#3").css('text-decoration', 'none');
-    }
+      
+    } 
 
-    else if(scroll_pos > 660&&scroll_pos < 850) {
+    var isElementInView = Utils.isElementInView($('#skills'), false);
+    if (isElementInView) {
+        console.log('skills in view');
+        $("#1").css('text-decoration', 'underline');
+        $("#2").css('text-decoration', 'none');
+        $("#3").css('text-decoration', 'none');
+        $("#4").css('text-decoration', 'none');
+        $("#5").css('text-decoration', 'none');
+        
+    } 
 
-      $("#1").css('text-decoration', 'underline');
-      $("#2").css('text-decoration', 'none');
-      $("#4").css('text-decoration', 'none');
-      $("#5").css('text-decoration', 'none');
-      $("#3").css('text-decoration', 'none');
-    }
-    else if(scroll_pos > 850&&scroll_pos < 1600) {
+    var isElementInView = Utils.isElementInView($('#AP'), false);
+    if (isElementInView) {
+      $("#1").css('text-decoration', 'none');
       $("#2").css('text-decoration', 'underline');
-      $("#1").css('text-decoration', 'none');
+      $("#3").css('text-decoration', 'none');
       $("#4").css('text-decoration', 'none');
       $("#5").css('text-decoration', 'none');
-      $("#3").css('text-decoration', 'none');
-    }
-    else if(scroll_pos > 1980 &&scroll_pos < 2300) {
+    } 
+
+
+    var isElementInView = Utils.isElementInView($('#PIP'), false);
+    if (isElementInView) {
       $("#1").css('text-decoration', 'none');
       $("#2").css('text-decoration', 'none');
-      $("#4").css('text-decoration', 'none');
-      $("#5").css('text-decoration', 'none');
-      $("#3").css('text-decoration', 'none');
-    }
-    else if(scroll_pos > 2300 && scroll_pos < 2800) {
-      $("#4").css('text-decoration', 'underline');
-      $("#1").css('text-decoration', 'none');
-      $("#2").css('text-decoration', 'none');
-      $("#5").css('text-decoration', 'underline');
       $("#3").css('text-decoration', 'underline');
-    }
+      $("#4").css('text-decoration', 'none');
+      $("#5").css('text-decoration', 'none');
+     
+    } 
+    var isElementInView = Utils.isElementInView($('#RC'), false);
+    if (isElementInView) {
+      $("#1").css('text-decoration', 'none');
+      $("#2").css('text-decoration', 'none');
+      $("#3").css('text-decoration', 'underline');
+      $("#4").css('text-decoration', 'underline');
+      $("#5").css('text-decoration', 'underline');
+    } 
+
+
+    // if(scroll_pos <450) {
+    //   $("#1").css('text-decoration', 'none');
+    //   $("#2").css('text-decoration', 'none');
+    //   $("#3").css('text-decoration', 'none');
+    //   $("#4").css('text-decoration', 'none');
+    //   $("#5").css('text-decoration', 'none');
+    // }
+
+    // else if(scroll_pos > 660&&scroll_pos < 850) {
+
+    //   $("#1").css('text-decoration', 'underline');
+    //   $("#2").css('text-decoration', 'none');
+    //   $("#4").css('text-decoration', 'none');
+    //   $("#5").css('text-decoration', 'none');
+    //   $("#3").css('text-decoration', 'none');
+    // }
+    // else if(scroll_pos > 850&&scroll_pos < 1600) {
+    //   $("#2").css('text-decoration', 'underline');
+    //   $("#1").css('text-decoration', 'none');
+    //   $("#4").css('text-decoration', 'none');
+    //   $("#5").css('text-decoration', 'none');
+    //   $("#3").css('text-decoration', 'none');
+    // }
+    // else if(scroll_pos > 1980 &&scroll_pos < 2300) {
+    //   $("#1").css('text-decoration', 'none');
+    //   $("#2").css('text-decoration', 'none');
+    //   $("#4").css('text-decoration', 'none');
+    //   $("#5").css('text-decoration', 'none');
+    //   $("#3").css('text-decoration', 'none');
+    // }
+    // else if(scroll_pos > 2300 && scroll_pos < 2800) {
+    //   $("#4").css('text-decoration', 'underline');
+    //   $("#1").css('text-decoration', 'none');
+    //   $("#2").css('text-decoration', 'none');
+    //   $("#5").css('text-decoration', 'underline');
+    //   $("#3").css('text-decoration', 'underline');
+    // }
 
   });
 
