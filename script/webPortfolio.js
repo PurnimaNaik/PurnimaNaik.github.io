f4734e5f902f72c1184f7720420c7fcca02e32be
PurnimaNaik <naikpurnima9@gmail.com>
2019-09-09 13:20:22 -0400

    Content edit from staging

--- script/webPortfolio.js
+++ script/webPortfolio.js
@@ -35,62 +35,40 @@ $(document).ready(function(){
 
       $("#1").css('text-decoration', 'none');
       $("#2").css('text-decoration', 'none');
-      $("#3").css('text-decoration', 'none');
       $("#4").css('text-decoration', 'none');
       $("#5").css('text-decoration', 'none');
-      $("#6").css('text-decoration', 'none');
+      $("#3").css('text-decoration', 'none');
     }
 
     else if(scroll_pos > 660&&scroll_pos < 850) {
 
       $("#1").css('text-decoration', 'underline');
       $("#2").css('text-decoration', 'none');
-      $("#3").css('text-decoration', 'none');
       $("#4").css('text-decoration', 'none');
       $("#5").css('text-decoration', 'none');
-      $("#6").css('text-decoration', 'none');
+      $("#3").css('text-decoration', 'none');
     }
     else if(scroll_pos > 850&&scroll_pos < 1600) {
       $("#2").css('text-decoration', 'underline');
       $("#1").css('text-decoration', 'none');
-      $("#3").css('text-decoration', 'none');
       $("#4").css('text-decoration', 'none');
       $("#5").css('text-decoration', 'none');
-      $("#6").css('text-decoration', 'none');
+      $("#3").css('text-decoration', 'none');
     }
     else if(scroll_pos > 1980 &&scroll_pos < 2300) {
-      $("#3").css('text-decoration', 'underline');
       $("#1").css('text-decoration', 'none');
       $("#2").css('text-decoration', 'none');
       $("#4").css('text-decoration', 'none');
       $("#5").css('text-decoration', 'none');
-      $("#6").css('text-decoration', 'none');
+      $("#3").css('text-decoration', 'none');
     }
     else if(scroll_pos > 2300 && scroll_pos < 2800) {
       $("#4").css('text-decoration', 'underline');
       $("#1").css('text-decoration', 'none');
-      $("#3").css('text-decoration', 'none');
       $("#2").css('text-decoration', 'none');
       $("#5").css('text-decoration', 'underline');
-      $("#6").css('text-decoration', 'underline');
+      $("#3").css('text-decoration', 'underline');
     }
-    // else if(scroll_pos > 2800 && scroll_pos < 3000) {
-    //           $("#5").css('text-decoration', 'underline');
-    //           $("#1").css('text-decoration', 'none');
-    //           $("#3").css('text-decoration', 'none');
-    //           $("#4").css('text-decoration', 'none');
-    //           $("#2").css('text-decoration', 'none');
-    //           $("#6").css('text-decoration', 'none');
-    // }
-    // else if(scroll_pos > 3000) {
-    //           $("#6").css('text-decoration', 'underline');
-    //           $("#1").css('text-decoration', 'none');
-    //           $("#3").css('text-decoration', 'none');
-    //           $("#4").css('text-decoration', 'none');
-    //           $("#5").css('text-decoration', 'none');
-    //           $("#2").css('text-decoration', 'none');
-    // }
-
 
   });
 
